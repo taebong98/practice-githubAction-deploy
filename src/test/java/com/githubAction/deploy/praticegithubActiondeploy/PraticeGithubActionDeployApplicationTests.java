a444986d61a6a28f3453267f0a1caed5db97526c
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-08 13:44:21 +0900

    start project

---
+++ src/test/java/com/githubAction/deploy/praticegithubActiondeploy/PraticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubAction.deploy.praticegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
